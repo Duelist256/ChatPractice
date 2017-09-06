d5b29de637688acbe412779d94ccbf6adbf901f3
Duelist <selykov.iw@yandex.ru>
2017-09-06 22:47:18 +0300

    Added classwork 07.05.2017

---
+++ src/day170705/NetClient.java
@@ -0,0 +1,26 @@
+package day170705;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class NetClient {
+    public static void main(String[] args) {
+
+        try {
+            Socket socket = new Socket("localhost", 10000);
+
+            Scanner scanner = new Scanner(socket.getInputStream());
+
+            while(scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                System.out.println(line);
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
